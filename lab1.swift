be486896e636e353ea5ba5acca170327ff3347c2
nieyrinn <kken77701@gmail.com>
2025-09-19 21:42:07 +0500

    lab1

--- lab1.swift
+++ lab1.swift
@@ -21,7 +21,12 @@ var favoriteFood = "Besh"
 var isInAlmaty = true
 
 //step3
-let text = "Hello, My name is \(firstName) \(lastName)! I am \(age) years old. My height is \(height) cm and I am currently \(isStudent ? "a" : "not a") student, I have \(numberOfHobbies) hobbies, one of them is \(hobby), which is \(isHobbyCreative ? "requires" : "not requires") creativity. I love eating \(favoriteFood) with my friend \(friend). My favorite number is \(favoriteNumber) and Im living \(isInAlmaty ? "in Almaty" : "in other city")."
+let text = """
+
+Hello, My name is \(firstName) \(lastName)! I am \(age) years old. My height is \(height) cm and I am currently \(isStudent ? "a" : "not a") student, 
+I have \(numberOfHobbies) hobbies, one of them is \(hobby), which is \(isHobbyCreative ? "requires" : "not requires") creativity. 
+I love eating \(favoriteFood) with my friend \(friend). My favorite number is \(favoriteNumber) and Im living \(isInAlmaty ? "in Almaty" : "in other city").
+"""
 
 //step4
 print(text)
@@ -31,4 +36,8 @@ var studyGoal = "upper scolarship"
 var futureGoal = "IOS developer💻"
 var 💻 = "games"
 
-print("In future,I want to get " + studyGoal + " and become " + futureGoal + " also create some " + 💻 + " I know that it will be hard, but doest mind if u want it and try hard😙")
+print("""
+In future,I want to get \(studyGoal) and become \(futureGoal) also create some \(💻)  
+I know that it will be hard, but doest mind if u want it and try hard😙
+
+""")
